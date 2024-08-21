cafd55757e821097260dff4f8eb6b62e627ec31f
maximmft <mmaufront@gmail.com>
2024-08-21 11:07:35 +0200

    quete done

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,7 @@
+// Import des dépendances et des styles
 import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
-import './styles/app.css';
+import './styles/app.scss';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Peut être retiré si tu utilises uniquement SCSS
+import 'bootstrap';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
